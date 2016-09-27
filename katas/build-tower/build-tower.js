b3ad47e60d3b3ea0b02c07ec82b3224d6a5dcaa3
ivanStraltsou <ivan_straltsou@epam.com>
2016-09-27 12:41:38 +0300

    back home, deps are updated

---
+++ katas/build-tower/build-tower.js
@@ -0,0 +1,17 @@
+function buildTower(numberOfFloors) {
+  /*var tower = Array.apply(null, new Array(nFloors));
+  var width = nFloors + 2 * (nFloors - 1) - (nFloors > 1 ? 1 : 0);
+  var lastRow = Array.apply(null, new Array(width)).map(function() {return '*';});
+  
+  tower.reduce(function(previousRow, value, index) {
+    if (index) {
+      previousRow[index - 1] = previousRow[width - index] = ' ';
+    }
+    
+    tower[index] = previousRow.join('');
+    
+    return previousRow;
+  }, lastRow);
+  
+  return tower.reverse();*/
+}
